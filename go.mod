c7f71c238e325d71c6983e23ef8843fc82b69dc8
dalan <863286+dalanmiller@users.noreply.github.com>
2022-10-23 22:20:54 +1100

    init

---
+++ go.mod
@@ -0,0 +1,69 @@
+module github.com/dalanmiller/markaway/v2
+
+go 1.19
+
+require (
+	github.com/charmbracelet/bubbles v0.14.0
+	github.com/charmbracelet/bubbletea v0.22.1
+	github.com/charmbracelet/glamour v0.2.1-0.20210402234443-abe9cda419ba
+	github.com/charmbracelet/glow v1.4.1
+	github.com/charmbracelet/lipgloss v0.6.0
+)
+
+require (
+	github.com/alecthomas/chroma v0.8.2 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/calmh/randomart v1.1.0 // indirect
+	github.com/charmbracelet/charm v0.8.6 // indirect
+	github.com/chris-ramon/douceur v0.2.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dlclark/regexp2 v1.2.0 // indirect
+	github.com/dustin/go-humanize v1.0.1-0.20200219035652-afde56e7acac // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/meowgorithm/babyenv v1.3.1 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.21 // indirect
+	github.com/mikesmitty/edkey v0.0.0-20170222072505-3356ea4e686a // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/gitcha v0.2.0 // indirect
+	github.com/muesli/go-app-paths v0.2.1 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/sasquatch v0.0.0-20200811221207-66979d92330a // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sabhiram/go-gitignore v0.0.0-20180611051255-d3107576ba94 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
+	github.com/segmentio/ksuid v1.0.3 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/cobra v1.1.3 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/yuin/goldmark v1.3.1 // indirect
+	github.com/yuin/goldmark-emoji v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20221002022538-bcab6841153b // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.51.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/stopwatch"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	initialInputs = 2
+	maxInputs     = 6
+	minInputs     = 1
+	titleHeight   = 3
+	helpHeight    = 5
+)
+
+var (
+	cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212"))
+
+	cursorLineStyle = lipgloss.NewStyle().
+			Background(lipgloss.Color("57")).
+			Foreground(lipgloss.Color("230"))
+
+	placeholderStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("238"))
+
+	endOfBufferStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("235"))
+
+	focusedPlaceholderStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("99"))
+
+	focusedBorderStyle = lipgloss.NewStyle().
+				Border(lipgloss.RoundedBorder()).
+				BorderForeground(lipgloss.Color("238"))
+
+	blurredBorderStyle = lipgloss.NewStyle().
+				Border(lipgloss.HiddenBorder())
+)
+
+type keymap = struct {
+	next, insertComponent, prev, add, remove, save, quit key.Binding
+}
+
+func newTextarea() textarea.Model {
+	t := textarea.New()
+	t.Prompt = ""
+	t.Placeholder = "Type something"
+	t.ShowLineNumbers = true
+	t.Cursor.Style = cursorStyle
+	t.FocusedStyle.Placeholder = focusedPlaceholderStyle
+	t.BlurredStyle.Placeholder = placeholderStyle
+	t.FocusedStyle.CursorLine = cursorLineStyle
+	t.FocusedStyle.Base = focusedBorderStyle
+	t.BlurredStyle.Base = blurredBorderStyle
+	t.FocusedStyle.EndOfBuffer = endOfBufferStyle
+	t.BlurredStyle.EndOfBuffer = endOfBufferStyle
+	t.KeyMap.DeleteWordBackward.SetEnabled(false)
+	t.KeyMap.LineNext = key.NewBinding(key.WithKeys("down"))
+	t.KeyMap.LinePrevious = key.NewBinding(key.WithKeys("up"))
+	t.Blur()
+	return t
+}
+
+type model struct {
+	width     int
+	height    int
+	keymap    keymap
+	help      help.Model
+	input     textarea.Model
+	viewport  viewport.Model
+	focus     int
+	stopwatch stopwatch.Model
+	title     string
+}
+
+func newModel() model {
+	m := model{
+		input:     newTextarea(),
+		viewport:  viewport.New(0, 0),
+		help:      help.New(),
+		title:     "A New File",
+		stopwatch: stopwatch.NewWithInterval(time.Second),
+		keymap: keymap{
+			quit: key.NewBinding(
+				key.WithKeys("esc", "ctrl+c", "cmd+q"),
+				key.WithHelp("esc", "quit"),
+			),
+			save: key.NewBinding(
+				key.WithKeys("ctrl+s", "cmd+s"),
+				key.WithHelp("ctrl+s", "save a file"),
+			),
+			insertComponent: key.NewBinding(
+				key.WithKeys("ctrl+i", "cmd+i"),
+				key.WithHelp("ctrl+i", "insert md component"),
+			),
+		},
+	}
+
+	m.updateKeybindings()
+	return m
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(
+		textarea.Blink,
+		m.stopwatch.Init(),
+	)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keymap.quit):
+			m.input.Blur()
+			return m, tea.Quit
+		case key.Matches(msg, m.keymap.save):
+			// saveFile()
+		default:
+			if !m.input.Focused() {
+				cmd := m.input.Focus()
+				cmds = append(cmds, cmd)
+
+			}
+		}
+
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
+		m.sizeInputs()
+	}
+
+	m.updateKeybindings()
+
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+		swCmd tea.Cmd
+	)
+	m.input, tiCmd = m.input.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+	m.stopwatch, swCmd = m.stopwatch.Update(msg)
+
+	cmds = append(cmds, tiCmd, vpCmd, swCmd)
+	return m, tea.Batch(cmds...)
+}
+
+func (m *model) sizeInputs() {
+	m.input.SetWidth(m.width / 2)
+	m.input.SetHeight(m.height - helpHeight - titleHeight)
+
+	m.viewport = viewport.New(m.width/2, m.height-helpHeight-titleHeight)
+}
+
+func (m *model) updateKeybindings() {
+	// m.keymap.add.SetEnabled(len(m.inputs) < maxInputs)
+	// m.keymap.remove.SetEnabled(len(m.inputs) > minInputs)
+}
+
+var (
+	L = lipgloss.Left
+	R = lipgloss.Right
+
+	titleStyle     = lipgloss.NewStyle().Bold(true).Align(L).Padding(1, 1)
+	bufferStyle    = lipgloss.NewStyle()
+	stopwatchStyle = lipgloss.NewStyle().Bold(true).Align(R).Padding(1, 1)
+)
+
+func (m model) View() string {
+	page := strings.Builder{}
+
+	title := titleStyle.Render(m.title)
+	sw := stopwatchStyle.Render(m.stopwatch.View())
+	buffer := bufferStyle.Width(m.width - lipgloss.Width(title) - lipgloss.Width(sw)).Render(" ")
+
+	titleBar := lipgloss.JoinHorizontal(
+		lipgloss.Top,
+		title,
+		buffer,
+		sw,
+	)
+	page.WriteString(titleBar)
+
+	help := m.help.ShortHelpView([]key.Binding{
+		m.keymap.next,
+		m.keymap.prev,
+		m.keymap.add,
+		m.keymap.remove,
+		m.keymap.quit,
+	})
+
+	renderedMarkdown, _ := glamour.Render(m.input.Value(), "dark")
+
+	m.viewport.SetContent(renderedMarkdown)
+
+	// Need to style left and right sides
+	// 1. have a gutter between
+	// 2. Nice padding
+	// 3. Highlight current line
+
+	page.WriteString("\n\n")
+	page.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, m.input.View(), m.viewport.View()))
+	page.WriteString("\n\n")
+	page.WriteString(help)
+	return page.String()
+}
+
+func main() {
+	if err := tea.NewProgram(newModel(), tea.WithAltScreen()).Start(); err != nil {
+		fmt.Println("Error while running program:", err)
+		os.Exit(1)
+	}
+}
